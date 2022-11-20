169cb84f4b39ac669621f440895011825fd7ff8a
Wenderson Santos <guto15santos@gmail.com>
2022-11-20 14:52:13 -0300

    14 - Tipos primitivos pt 05 - Exercício

---
+++ src/academy/devdojo/maratonajava/introducao/Aua3TiposPrimitivosExercicio.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aua3TiposPrimitivosExercicio {
+
+    String nome = "Gustavo";
+    String endereco = "Tv Pe Antonio Franco";
+    double salario = 54321.12D;
+    String dataRecebimentoSalario = "20/12/2021";
+    }
