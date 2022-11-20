8f215b446cb461744076dc524f43177916429a31
Wenderson Santos <guto15santos@gmail.com>
2022-11-20 14:38:55 -0300

    11 - Tipos primitivos pt 02 - declaração e tamanho em memória

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,5 +3,18 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, boolean, byte, short, char e long
+
+        int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDOuble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 87; // Tabela ASC 87 é W
+
+
+        System.out.println(caractere);
     }
 }
