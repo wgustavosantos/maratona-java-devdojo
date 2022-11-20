00207b456b4c70b8985c3d4bf7302d43e50d9f17
Wenderson Santos <guto15santos@gmail.com>
2022-11-20 15:01:20 -0300

    15 - Operadores pt 01 - Aritiméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+        System.out.println(numero1 + numero2 + " antes da String é operador aritmetico " + numero1 + numero2 );
+
+
+    }
+}
