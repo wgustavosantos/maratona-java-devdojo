484fe612956a80d27871a9b8a2d05cc19a5d2720
Wenderson Santos <guto15santos@gmail.com>
2022-11-20 07:57:15 -0300

    09 - Comentários

--- src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,8 +1,18 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Isto é um comentário <b>javadoc</b>
+ */
 public class OlaDevDojo {
 
     public static void main(String[] args) {
+        // Isto é um comentário de uma linha
+
+        /*
+            Isto é um comentário
+            de multiplas
+            linhas
+         */
         System.out.println("Olá mundo");
     }
 }
