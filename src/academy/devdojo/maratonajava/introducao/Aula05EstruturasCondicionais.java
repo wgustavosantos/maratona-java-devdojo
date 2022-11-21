91aa4deda198c6d63e6ffdd893638927cbb5040f
Wenderson Santos <guto15santos@gmail.com>
2022-11-21 17:34:12 -0300

    22 - Estruturas Condicionais pt 03 - Operador ternário

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -6,13 +6,16 @@ public class Aula05EstruturasCondicionais {
         int idade = 15;
         String categoria = null;
 
-        if(idade < 12){
-            categoria = "Juvenil";
-        } else if (idade > 18) {
-            categoria = "Adulto";
-        }
+//        if(idade < 12){
+//            categoria = "Juvenil";
+//        } else if (idade >= 15 && idade < 18) {
+//            categoria = "Categoria Juvenil";
+//        } else {
+//            categoria = "categoria adulto";
+//        }
+
+        categoria = idade < 15 ? "Categoria infantil" : idade >= 15 && idade < 18 ? "Categoria Juvenil" : "Categoria Adulto";
         
         System.out.println(categoria);
-
     }
 }
