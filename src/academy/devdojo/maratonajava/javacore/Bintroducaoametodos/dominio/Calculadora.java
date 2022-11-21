a2ec45c40f529545790a9f41eb681f7d42491a34
Wenderson Santos <guto15santos@gmail.com>
2022-11-21 20:02:30 -0300

    44 - Orientação Objetos - Métodos pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaoametodos/dominio/Calculadora.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaoametodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaoametodos/test/CalculadoraTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaoametodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaoametodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+    }
+}
