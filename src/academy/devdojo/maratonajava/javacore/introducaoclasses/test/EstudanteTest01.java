1796aeb389cf70c3c1f8a1e47aac4b7cbc4d4537
Wenderson Santos <guto15santos@gmail.com>
2022-11-21 17:54:30 -0300

    39 - Orientação Objetos pt 01 - Introdução classes pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+    }
+}
