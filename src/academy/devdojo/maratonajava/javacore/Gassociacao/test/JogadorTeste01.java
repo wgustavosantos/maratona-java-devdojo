c15ef51405b6c83a53c714819eb0f689d7e1d7aa
Wenderson Santos <guto15santos@gmail.com>
2022-11-24 17:54:34 -0300

    64 - Orientação Objetos - Associação pt 01 - Arrays com Objetos

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTeste01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTeste01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pele");
+        Jogador jogador2 = new Jogador("Romario");
+        Jogador jogador3 = new Jogador("Kaka");
+
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+        // Jogador[] jogadores = new Jogador[] {jogador1, jogador2, jogador3};
+
+
+
+    }
+}
