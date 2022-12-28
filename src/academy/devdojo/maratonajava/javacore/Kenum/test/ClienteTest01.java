eb7baf20edfc91e9d461a7b2bca19354a1acdc71
Wenderson Santos <guto15santos@gmail.com>
2022-12-28 15:19:33 -0300

    80 - Orientação Objetos - Enumeração pt 01 - Introdução

---
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Kenum.test;
+
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+
+public class ClienteTest01 {
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente("Gustavo", TipoCliente.PESSOA_JURIDICA);
+    }
+}
