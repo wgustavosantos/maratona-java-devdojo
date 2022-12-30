fe6d4f74f37283696d3c3ed541e5e739abed7747
Wenderson Santos <guto15santos@gmail.com>
2022-12-30 14:12:57 -0300

    91 - Orientação Objetos - Polimorfismo pt 02 - Funcionamento

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Positivo", 2000);
+        Produto produto2 = new Tomate("Ciciliano", 10);
+
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("*-------------------------------*");
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+
+    }
+}
