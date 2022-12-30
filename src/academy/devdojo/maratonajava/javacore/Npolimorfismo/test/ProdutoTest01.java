c22e9eb6056760ff0ca53206f84423be16357f13
Wenderson Santos <guto15santos@gmail.com>
2022-12-30 10:43:43 -0300

    90 - Orientação Objetos - Polimorfismo pt 01 - Introdução

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest01 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("Positivo", 2000);
+        Tomate tomate = new Tomate("Ciciliano", 10);
+
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+        System.out.println("*-------------------------------*");
+        CalculadoraImposto.calcularImpostoComputador(computador);
+    }
+}
