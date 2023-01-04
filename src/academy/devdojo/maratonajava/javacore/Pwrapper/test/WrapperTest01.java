927e72a345b2358ecc2cbb1991157dc3b3a6549e
Wenderson Santos <guto15santos@gmail.com>
2023-01-04 13:16:14 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+
+        Byte Wbyte = 127;
+        Short WShort = 1;
+        Integer WInteger = 1;
+        Long WLong = 1000L;
+        Float WFloat = 10F;
+        Double WDouble = 10D;
+        Character WChar = 'W';
+        Boolean WBoolean = true;
+
+    }
+}
