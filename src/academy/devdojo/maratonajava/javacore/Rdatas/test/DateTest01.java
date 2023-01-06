b178dca7d15c7200ec7d79bfa7da536879136c52
Wenderson Santos <guto15santos@gmail.com>
2023-01-06 09:51:47 -0300

    112 - Classes Utilitárias - Date

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1_000_000_000_00L);
+        System.out.println(date);
+
+    }
+}
