fa030e96857d20488e220f2e7dcea7c0cbc658d4
Wenderson Santos <guto15santos@gmail.com>
2023-01-09 10:09:38 -0300

    123 - Classes Utilitárias - Duration

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2);
+    }
+}
