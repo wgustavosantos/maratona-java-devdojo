a2cbd9a2d0c03635f345e878a7a4daab2a23b3a8
Wenderson Santos <guto15santos@gmail.com>
2023-01-09 10:34:45 -0300

    124 - Classes Utilitárias - Period

---
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.Period;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+        Period
+
+    }
+}
