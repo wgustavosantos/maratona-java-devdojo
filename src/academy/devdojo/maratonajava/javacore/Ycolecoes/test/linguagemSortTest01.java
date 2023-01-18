1f6643e2ac265b5e91bc52047ab5099c733b2a22
Wenderson Santos <guto15santos@gmail.com>
2023-01-18 15:28:00 -0300

    171 - Coleções pt 11 - Sorting lists pt 03 - Comparator

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/linguagemSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/linguagemSortTest01.java
@@ -4,8 +4,24 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Linguagem;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+class LinguagemBySalario implements Comparator<Linguagem>{
+
+    @Override
+    public int compare(Linguagem l1, Linguagem l2) {
+        // if(this < 0) return negative
+        // if(this == o) return zero
+        // if(this > o) return positive
+        if(l1.getSalario() < l2.getSalario())
+            return -1;
+        else if(l1.getSalario() > l2.getSalario())
+            return 1;
+        else
+            return 0;
+    }
+}
 public class linguagemSortTest01 {
     public static void main(String[] args) {
 
@@ -16,6 +32,7 @@ public class linguagemSortTest01 {
         linguagens.add(new Linguagem(3L, "Ruby", 6.850D));
 
         Collections.sort(linguagens);
+        Collections.sort(linguagens, new LinguagemBySalario());
 
         for (Linguagem linguagen : linguagens) {
             System.out.println(linguagen);
