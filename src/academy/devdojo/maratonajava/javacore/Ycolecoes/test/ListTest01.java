b2f46dc1a872b372f3795f2a2451602bca5d3675
Wenderson Santos <guto15santos@gmail.com>
2023-01-17 22:35:36 -0300

    167 - Coleções pt 07 - List pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,9 +6,15 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
 
         nomes.add("Gustavo");
         nomes.add("Gabriele");
+
+        nomes2.add("Wenderson");
+        nomes2.add("Gabi");
+
+        nomes.addAll(nomes2);
 
         for (String nome:nomes) {
             System.out.println(nome);
