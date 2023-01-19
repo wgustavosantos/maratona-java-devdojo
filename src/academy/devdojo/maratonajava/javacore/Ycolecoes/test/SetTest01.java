1a54b7fca83ba0c43720d16b1d49150e31835fe7
Wenderson Santos <guto15santos@gmail.com>
2023-01-18 21:44:19 -0300

    175 - Coleções pt 15 - Set, HashSet

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SetTest01.java
@@ -3,26 +3,22 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Linguagem;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class IteratorTest01 {
+public class SetTest01 {
     public static void main(String[] args) {
+        Set<Linguagem> linguagens = new HashSet<>();
 
-        List<Linguagem> linguagens = new ArrayList<>();
         linguagens.add(new Linguagem(2L, "BASIC", 5.250D, 1975 ));
         linguagens.add(new Linguagem(1L, "Java", 8.850D, 1995));
 
         linguagens.add(new Linguagem(3L, "Ruby", 6.850D, 1999));
 
-        Iterator<Linguagem> i = linguagens.iterator();
-
-//        while(i.hasNext()){
-//            if(i.next().getAno() > 1990)
-//                i.remove();
-//        }
-        linguagens.removeIf(l -> l.getAno() > 1990);
-        System.out.println(linguagens);
+        for (Linguagem linguagen : linguagens) {
+            System.out.println(linguagen);
+        }
 
     }
 }
