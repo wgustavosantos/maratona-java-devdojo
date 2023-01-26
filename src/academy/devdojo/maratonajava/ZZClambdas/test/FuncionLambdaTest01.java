5e8afb67b9e4c2a8125b44628e6535370d9f2a07
Wenderson Santos <guto15santos@gmail.com>
2023-01-26 14:25:04 -0300

    197 - Lambdas pt 03 - Function

---
+++ src/academy/devdojo/maratonajava/ZZClambdas/test/FuncionLambdaTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.ZZClambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FuncionLambdaTest01 {
+    public static void main(String[] args) {
+
+        List<String> nomes = List.of("Gustavo", "Joice", "Leandro", "Gabrieli", "taylor");
+
+        final List<Integer> numeLetrasNome = map(nomes, String::length);
+
+        System.out.println(numeLetrasNome);
+    }
+
+    public static <T, R> List<R> map (List<T> objList, Function<T, R> function){
+        List<R> list = new ArrayList<>();
+
+        for(T t: objList){
+            final R r = function.apply(t);
+
+            list.add(r);
+        }
+        return list;
+    }
+}
