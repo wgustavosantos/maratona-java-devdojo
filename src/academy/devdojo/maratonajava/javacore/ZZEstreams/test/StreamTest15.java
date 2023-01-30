7a0d45984e4f146c61927c70cc032cf2c3e699f5
Wenderson Santos <guto15santos@gmail.com>
2023-01-30 16:27:16 -0300

    217 - Streams pt 15 - Collectors pt 05 - Grouping by pt 04

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest15.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Category;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+
+public class StreamTest15 {
+    private static List<LightNovel> list = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)));
+    public static void main(String[] args) {
+        /*Fazer agrupamento por categoria e ter a estatistica de cada categoria*/
+        final Map<Category, DoubleSummaryStatistics> collect = list.stream().collect(groupingBy(LightNovel::getCategory, Collectors.summarizingDouble(LightNovel::getPrice)));
+        System.out.println(collect);
+    }
+}
